29ee5eeb86b492a1b0305204880e50209b4ba1f8
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2024-10-01 15:18:06 +0530

    Update template.c

--- template.c
+++ template.c
@@ -4,4 +4,5 @@ ADD(int a, int b){return a+b;}
 SUB(int a, int b){return a-b;}
 MULT(int a, int b){return a*b;}
 DIV(int a, int b){return a*b;}
+  printf("Hello everyone this is your last day on earth")
 }
